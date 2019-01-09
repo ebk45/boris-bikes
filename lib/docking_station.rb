978d1d3831b767827c1d64e09107944a7225fa0d
ellie turnock <ebturnock@gmail.com>
2019-01-09 14:41:31 +0000

    added bike spec file

---
+++ lib/docking_station.rb
@@ -0,0 +1,12 @@
+class DockingStation
+
+  def release_bike
+  end
+end
+
+class Bike
+
+  def working?
+  end
+  
+end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,2 +1,7 @@
+require 'docking_station.rb'
+
 describe DockingStation do
+    it { is_expected.to respond_to(:release_bike) }
 end
+#parentheses are not necessary, put space between respond_to
+#and release bike.
